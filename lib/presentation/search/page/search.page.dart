53c820380c2d6913dbf28a66dd5459896d02de88
ooeunoo <seongeun.cho.dev@gmail.com>
2024-11-07 12:09:28 +0900

    feat: search page

--- lib/presentation/search/page/search.page.dart
+++ lib/presentation/search/page/search.page.dart
@@ -12,83 +12,162 @@ class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
 
   @override
-  _SearchPageState createState() => _SearchPageState();
+  State<SearchPage> createState() => _SearchPageState();
 }
 
 class _SearchPageState extends State<SearchPage> {
+  bool _showSearchBar = false;
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppHeader(
-        title: AppText('실시간 검색 순위',
-            style: Theme.of(context).textTheme.textXL.copyWith(
-                color: AppColor.gray100, fontWeight: AppFontWeight.medium)),
-        automaticallyImplyLeading: false,
-        leading: const SizedBox.shrink(),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.search, color: AppColor.gray100),
-          ),
-        ],
-      ),
+      appBar: _buildAppBar(),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: _showSearchBar ? _buildSearchContent() : _buildRankContent(),
+      ),
+    );
+  }
+
+  Widget _buildSearchContent() {
+    return Container(); // 검색 결과를 표시할 위젯 구현 예정
+  }
+
+  Widget _buildRankContent() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildLastUpdateText(),
+        AppSpacerV(value: AppDimens.height(20)),
+        _buildCategoryTabList(),
+        AppSpacerV(value: AppDimens.height(20)),
+        _buildSearchRankList(),
+      ],
+    );
+  }
+
+  PreferredSizeWidget _buildAppBar() {
+    return _showSearchBar ? _buildSearchAppBar() : _buildDefaultAppBar();
+  }
+
+  AppBar _buildSearchAppBar() {
+    return AppBar(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back, color: AppColor.gray100),
+        onPressed: () {
+          setState(() {
+            _showSearchBar = false;
+          });
+        },
+      ),
+      title: TextField(
+        controller: _searchController,
+        style: Theme.of(context)
+            .textTheme
+            .textSM
+            .copyWith(color: AppColor.gray100),
+        decoration: InputDecoration(
+          hintText: '검색어를 입력하세요',
+          hintStyle: Theme.of(context)
+              .textTheme
+              .textSM
+              .copyWith(color: AppColor.gray400),
+          border: InputBorder.none,
+        ),
+        autofocus: true,
+      ),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.clear, color: AppColor.gray100),
+          onPressed: () {
+            if (_searchController.text.isEmpty) {
+              setState(() {
+                _showSearchBar = false;
+              });
+            } else {
+              _searchController.clear();
+            }
+          },
+        ),
+      ],
+    );
+  }
+
+  AppHeader _buildDefaultAppBar() {
+    return AppHeader(
+      title: AppText(
+        '실시간 검색 순위',
+        style: Theme.of(context).textTheme.textXL.copyWith(
+            color: AppColor.gray100, fontWeight: AppFontWeight.medium),
+      ),
+      automaticallyImplyLeading: false,
+      leading: const SizedBox.shrink(),
+      actions: [
+        IconButton(
+          onPressed: () {
+            setState(() {
+              _showSearchBar = true;
+            });
+          },
+          icon: const Icon(Icons.search, color: AppColor.gray100),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildLastUpdateText() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: AppDimens.width(20)),
+      child: AppText(
+        '5분 전 업데이트',
+        style: Theme.of(context).textTheme.textXS.copyWith(
+              color: AppColor.gray400,
+            ),
+      ),
+    );
+  }
+
+  Widget _buildCategoryTabList() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: AppDimens.width(20)),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
           children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: AppDimens.width(20)),
-              child: AppText(
-                '5분 전 업데이트',
-                style: Theme.of(context).textTheme.textXS.copyWith(
-                      color: AppColor.gray400,
-                    ),
-              ),
-            ),
-
-            AppSpacerV(value: AppDimens.height(20)),
-            // Category Tabs
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: AppDimens.width(20)),
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    _buildCategoryTab('전체', isActive: true),
-                    AppSpacerH(value: AppDimens.width(10)),
-                    _buildCategoryTab('뉴스'),
-                    AppSpacerH(value: AppDimens.width(10)),
-                    _buildCategoryTab('엔터'),
-                    AppSpacerH(value: AppDimens.width(10)),
-                    _buildCategoryTab('스포츠'),
-                    AppSpacerH(value: AppDimens.width(10)),
-                    _buildCategoryTab('경제'),
-                    AppSpacerH(value: AppDimens.width(10)),
-                    _buildCategoryTab('생활'),
-                  ],
-                ),
-              ),
-            ),
-
-            AppSpacerV(value: AppDimens.height(20)),
-            Expanded(
-              child: ListView(
-                children: [
-                  _buildSearchRankItem(
-                      1, '인공지능 챗봇', '새로운 AI 서비스 출시로 화제', '▲ 3'),
-                  _buildSearchRankItem(
-                      2, '메타버스 콘서트', '유명 아이돌 그룹 가상 공연 예정', '▼ 1'),
-                  _buildSearchRankItem(
-                      3, '친환경 에너지', '새로운 정부 정책 발표로 관심 증가', '▲ 5'),
-                  _buildSearchRankItem(
-                      4, '우주 탐사 미션', '민간 기업의 화성 탐사 계획 공개', '▲ 2'),
-                  _buildSearchRankItem(
-                      5, '글로벌 경제 전망', '주요 기관의 2024년 경제 예측 발표', '▼ 2'),
-                ],
-              ),
-            ),
+            _buildCategoryTab('전체', isActive: true),
+            AppSpacerH(value: AppDimens.width(10)),
+            _buildCategoryTab('뉴스'),
+            AppSpacerH(value: AppDimens.width(10)),
+            _buildCategoryTab('엔터'),
+            AppSpacerH(value: AppDimens.width(10)),
+            _buildCategoryTab('스포츠'),
+            AppSpacerH(value: AppDimens.width(10)),
+            _buildCategoryTab('경제'),
+            AppSpacerH(value: AppDimens.width(10)),
+            _buildCategoryTab('생활'),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildSearchRankList() {
+    return Expanded(
+      child: ListView(
+        children: [
+          _buildSearchRankItem(1, '인공지능 챗봇', '새로운 AI 서비스 출시로 화제', '▲ 3'),
+          _buildSearchRankItem(2, '메타버스 콘서트', '유명 아이돌 그룹 가상 공연 예정', '▼ 1'),
+          _buildSearchRankItem(3, '친환경 에너지', '새로운 정부 정책 발표로 관심 증가', '▲ 5'),
+          _buildSearchRankItem(4, '우주 탐사 미션', '민간 기업의 화성 탐사 계획 공개', '▲ 2'),
+          _buildSearchRankItem(5, '글로벌 경제 전망', '주요 기관의 2024년 경제 예측 발표', '▼ 2'),
+        ],
       ),
     );
   }
@@ -126,50 +205,60 @@ class _SearchPageState extends State<SearchPage> {
       ),
       child: Row(
         children: [
-          AppText(
-            '$rank',
-            style: Theme.of(context).textTheme.textXL.copyWith(
-                  color: rank == 1
-                      ? AppColor.orange400
-                      : rank == 2
-                          ? AppColor.yellow400
-                          : AppColor.gray500,
-                ),
-          ),
+          _buildRankNumber(rank),
           AppSpacerH(value: AppDimens.width(10)),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                AppText(
-                  title,
-                  style: Theme.of(context).textTheme.textSM.copyWith(
-                        color: Colors.white,
-                      ),
-                ),
-                AppSpacerV(value: AppDimens.height(5)),
-                AppText(
-                  description,
-                  style: Theme.of(context).textTheme.textXS.copyWith(
-                        color: AppColor.gray400,
-                      ),
-                ),
-              ],
-            ),
+          _buildSearchItemContent(title, description),
+          _buildDeltaIndicator(delta),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildRankNumber(int rank) {
+    return AppText(
+      '$rank',
+      style: Theme.of(context).textTheme.textXL.copyWith(
+            color: rank == 1
+                ? AppColor.orange400
+                : rank == 2
+                    ? AppColor.yellow400
+                    : AppColor.gray500,
           ),
+    );
+  }
+
+  Widget _buildSearchItemContent(String title, String description) {
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
           AppText(
-            delta,
+            title,
+            style: Theme.of(context).textTheme.textSM.copyWith(
+                  color: Colors.white,
+                ),
+          ),
+          AppSpacerV(value: AppDimens.height(5)),
+          AppText(
+            description,
             style: Theme.of(context).textTheme.textXS.copyWith(
-                  fontFamily: 'Arial',
-                  fontSize: 14,
-                  color: delta.startsWith('▲')
-                      ? AppColor.blue400
-                      : AppColor.rose400,
+                  color: AppColor.gray400,
                 ),
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildDeltaIndicator(String delta) {
+    return AppText(
+      delta,
+      style: Theme.of(context).textTheme.textXS.copyWith(
+            fontFamily: 'Arial',
+            fontSize: 14,
+            color: delta.startsWith('▲') ? AppColor.blue400 : AppColor.rose400,
+          ),
     );
   }
 }
