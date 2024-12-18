75080c2ed5a62cc2c92d05db080f8c5ef56b00c0
ooeunoo <seongeun.cho.dev@gmail.com>
2024-12-18 18:40:50 +0900

    feat: search reasum

--- lib/presentation/search/controller/search.controller.dart
+++ lib/presentation/search/controller/search.controller.dart
@@ -8,7 +8,7 @@ import 'package:zippy/domain/model/params/get_tranding_keywords.params.dart';
 import 'package:zippy/domain/usecases/get_articles_by_keyword.usecase.dart';
 import 'package:zippy/domain/usecases/get_trending_keywords.usecase.dart';
 
-class AppSearchController extends GetxController {
+class AppSearchController extends SuperController {
   final AuthService authService = Get.find();
   final ArticleService articleService = Get.find();
 
@@ -25,6 +25,11 @@ class AppSearchController extends GetxController {
     await _initialize();
   }
 
+  @override
+  void onResumed() {
+    _initialize();
+  }
+
   ///*********************************
   /// Initialization Methods
   ///*********************************
@@ -34,9 +39,7 @@ class AppSearchController extends GetxController {
 
   Future<void> _fetchTrendingKeywords() async {
     const params = GetTrandingKeywordsParams(contentType: null);
-    print("params: $params");
     final result = await getTrendingKeywords.execute(params);
-    print("result: $result");
     result.fold(
         (l) => null, (keywords) => trendingKeywords.assignAll(keywords));
   }
@@ -44,7 +47,6 @@ class AppSearchController extends GetxController {
   ///*********************************
   /// Public Methods
   ///*********************************
-
   Future<List<Article>> onHandleFetchArticlesBySearch(String keyword) async {
     final params = GetSearchArticlesParams(query: keyword);
     final result = await articleService.onHandleFetchSearchArticles(params);
@@ -54,5 +56,25 @@ class AppSearchController extends GetxController {
 
   Future<void> onHandleClickArticle(Article article) async {
     articleService.onHandleGoToArticleView(article);
+  }
+
+  @override
+  void onDetached() {
+    // TODO: implement onDetached
+  }
+
+  @override
+  void onHidden() {
+    // TODO: implement onHidden
+  }
+
+  @override
+  void onInactive() {
+    // TODO: implement onInactive
+  }
+
+  @override
+  void onPaused() {
+    // TODO: implement onPaused
   }
 }
